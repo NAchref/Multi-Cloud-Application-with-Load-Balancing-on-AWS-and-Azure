716435a75843dae2e40a948525e1c53b64d63c71
NAchref <achref.nefzaoui@esprit.tn>
2023-05-07 23:23:50 +0100

    Config urls

--- SPRINT 1 - The Application/CarsEcommerce/urls.py
+++ SPRINT 1 - The Application/CarsEcommerce/urls.py
@@ -16,10 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import path, include
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('store.urls')),
 
 ]
 
--- SPRINT 1 - The Application/store/views.py
+++ SPRINT 1 - The Application/store/views.py
@@ -3,6 +3,15 @@ from django.shortcuts import render
 
 #create your views here
 
+
 def store(request):
-    context = {}
-    return render(request, 'store/store.html')
+     context = {}
+     return render(request, 'store/store.html', context)
+
+def cart(request):
+     context = {}
+     return render(request, 'store/cart.html', context)
+
+def checkout(request):
+      context = {}
+      return render(request, 'store/checkout.html', context)
