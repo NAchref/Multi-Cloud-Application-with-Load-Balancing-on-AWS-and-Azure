f4fc5a585435002dd3e6ac3522fcb530e78f9ec2
NAchref <achref.nefzaoui@esprit.tn>
2023-05-26 16:04:46 +0100

    navbar test design

--- SPRINT 1 - The Application/store/views.py
+++ SPRINT 1 - The Application/store/views.py
@@ -3,9 +3,6 @@ from django.shortcuts import render
 
 #create your views here
 
-def store(request):
-     context = {}
-     return render(request, 'store/main.html', context)
 
 def store(request):
      context = {}
