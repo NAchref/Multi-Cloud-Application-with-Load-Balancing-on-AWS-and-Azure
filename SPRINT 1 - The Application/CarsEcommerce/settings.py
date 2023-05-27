029da4381a18e7d9f8d69cf9b358f7acf4be99db
NAchref <achref.nefzaoui@esprit.tn>
2023-05-27 18:59:49 +0100

    add livereload

--- SPRINT 1 - The Application/CarsEcommerce/settings.py
+++ SPRINT 1 - The Application/CarsEcommerce/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'livereload',
 
     'store.apps.StoreConfig',
 
@@ -51,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'livereload.middleware.LiveReloadScript',
 ]
 
 ROOT_URLCONF = 'CarsEcommerce.urls'
