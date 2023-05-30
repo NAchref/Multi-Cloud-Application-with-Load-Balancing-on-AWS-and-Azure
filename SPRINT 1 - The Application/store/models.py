044583b26c7ccaac01c7dd9facafee6c997bda31
NAchref <achref.nefzaoui@esprit.tn>
2023-05-30 15:21:34 +0100

    Data models creat

--- SPRINT 1 - The Application/store/models.py
+++ SPRINT 1 - The Application/store/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+#create your models here.
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, en_delete=models.CASCADE,  null=True, blank=True)
+    name = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200, null=True)
+
+    def __str__(self): 
+      return self.name
+    
+class Product(models.Models):
+   name = models.CharField(max_length=200, null=True)
+   price = models.FloatField()
+   digital = models.BooleanField(default=False, null=True, blank=False)
+   
+   def __str__(self):
+      return self.name
+
+
+
