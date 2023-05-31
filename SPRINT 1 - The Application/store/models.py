41c001f539e15fd0b781f3ff6719e20fe352983e
NAchref <achref.nefzaoui@esprit.tn>
2023-05-31 23:42:40 +0100

    make migrations

--- SPRINT 1 - The Application/store/models.py
+++ SPRINT 1 - The Application/store/models.py
@@ -28,6 +28,29 @@ class Order(models.Model):
    def __str__(self):
       return str(self.id)
    
+class OrderItem(models.Model):
+   product = models.ForeignKey(Product, on_delete=models.SET_NULL, blank=True, null=True)
+   order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
+   quantity = models.IntegerField(default=0, null=True, blank=True)
+   date_added = models.DateTimeField(auto_now_add=True)
+
+class ShippingAddress(models.Model):
+   customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
+   order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
+   address = models.CharField(max_length=200, null=False)
+   city = models.CharField(max_length=200, null=False)
+   state = models.CharField(max_length=200, null=False)
+   zipcode = models.CharField(max_length=200, null=False)
+   date_added = models.DateTimeField(auto_now_add=True)
+
+   def __str__(self):
+      return self.address
+   
+
+
+      
+
+
 
 
 
