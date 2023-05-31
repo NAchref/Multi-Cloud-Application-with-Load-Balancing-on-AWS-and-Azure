d980c6d44cdd183f2a9fb5f99804ce789008853a
NAchref <achref.nefzaoui@esprit.tn>
2023-05-31 23:30:51 +0100

    data base order

--- SPRINT 1 - The Application/store/models.py
+++ SPRINT 1 - The Application/store/models.py
@@ -4,20 +4,31 @@ from django.contrib.auth.models import User
 #create your models here.
 
 class Customer(models.Model):
-    user = models.OneToOneField(User, en_delete=models.CASCADE,  null=True, blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE,  null=True, blank=True)
     name = models.CharField(max_length=200, null=True)
     email = models.CharField(max_length=200, null=True)
 
     def __str__(self): 
       return self.name
     
-class Product(models.Models):
+class Product(models.Model):
    name = models.CharField(max_length=200, null=True)
    price = models.FloatField()
    digital = models.BooleanField(default=False, null=True, blank=False)
    
    def __str__(self):
       return self.name
+
+class Order(models.Model):
+   customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
+   date_orderd = models.DateTimeField(auto_now_add=True)
+   complete = models.BooleanField(default=False, null=True, blank=False)
+   transaction_id = models.CharField(max_length=200, null=True)
+
+   def __str__(self):
+      return str(self.id)
+   
+
 
 
 
