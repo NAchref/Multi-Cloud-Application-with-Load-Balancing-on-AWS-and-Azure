0f69ddc0a774cbd7777401ff5a9f425d11aa73f7
NAchref <achref.nefzaoui@esprit.tn>
2023-06-01 10:52:27 +0100

    add images to product models

--- SPRINT 1 - The Application/CarsEcommerce/settings.py
+++ SPRINT 1 - The Application/CarsEcommerce/settings.py
@@ -124,6 +124,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
+MEDIA_URL = '/images/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
--- SPRINT 1 - The Application/store/models.py
+++ SPRINT 1 - The Application/store/models.py
@@ -15,9 +15,23 @@ class Product(models.Model):
    name = models.CharField(max_length=200, null=True)
    price = models.FloatField()
    digital = models.BooleanField(default=False, null=True, blank=False)
+   image = models.ImageField(null=True, blank=True)
+
    
    def __str__(self):
       return self.name
+   
+   @property
+   def imageURL(self):
+      try:
+         url = self.image.url
+      except:
+         url = ''
+      return url
+      	
+			
+
+	
 
 class Order(models.Model):
    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
