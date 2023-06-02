a9b9edf9b91e86dbe5f70ef70c1469d76b2efd95
NAchref <achref.nefzaoui@esprit.tn>
2023-06-02 10:54:05 +0100

    render data to cart page

--- SPRINT 1 - The Application/store/views.py
+++ SPRINT 1 - The Application/store/views.py
@@ -11,7 +11,14 @@ def store(request):
      return render(request, 'store/store.html', context)
 
 def cart(request):
-     context = {}
+     if request.user.is_authenticated:
+          customer = request.user.customer
+          order, created = Order.objects.get_or_create(customer=customer, complete=False)
+          items = order.orderitem_set.all()
+     else:
+          items=[]     
+
+     context = {'items':items}
      return render(request, 'store/cart.html', context)
 
 def checkout(request):
