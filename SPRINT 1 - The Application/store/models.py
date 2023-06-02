9ebe04f0c70f432873165570e990bb38adbfe6bd
NAchref <achref.nefzaoui@esprit.tn>
2023-06-02 11:04:19 +0100

    render total by fk proprety

--- SPRINT 1 - The Application/store/models.py
+++ SPRINT 1 - The Application/store/models.py
@@ -48,6 +48,11 @@ class OrderItem(models.Model):
    quantity = models.IntegerField(default=0, null=True, blank=True)
    date_added = models.DateTimeField(auto_now_add=True)
 
+   @property
+   def get_total(self):
+      total = self.product.price * self.quantity
+      return total
+
 class ShippingAddress(models.Model):
    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
    order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
