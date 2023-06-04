243239def6d8dcea32c6606f09e2cf521dbcca30
NAchref <achref.nefzaoui@esprit.tn>
2023-06-04 12:18:27 +0100

    Checkout get total and get items

--- SPRINT 1 - The Application/store/views.py
+++ SPRINT 1 - The Application/store/views.py
@@ -23,5 +23,14 @@ def cart(request):
      return render(request, 'store/cart.html', context)
 
 def checkout(request):
-      context = {}
-      return render(request, 'store/checkout.html', context)
+     if request.user.is_authenticated:
+          customer = request.user.customer
+          order, created = Order.objects.get_or_create(customer=customer, complete=False)
+          items = order.orderitem_set.all()
+     else:
+          items=[]
+          order = {'get_cart_total':0, 'get_cart_items':0}  
+
+
+     context = {'items':items, 'order':order}
+     return render(request, 'store/checkout.html', context)
