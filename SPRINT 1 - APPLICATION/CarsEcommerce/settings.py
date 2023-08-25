1c0fc4dfbc6a71093feff02495ae50c2ed5c207c
NAchref <achref.nefzaoui@esprit.tn>
2023-08-25 17:23:40 +0100

    update

--- SPRINT 1 - APPLICATION/CarsEcommerce/settings.py
+++ SPRINT 1 - APPLICATION/CarsEcommerce/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7#bx9$-yh==%epr-2dfvk9bm4-3-ks4c83w*ili!-)*+c7(^y&'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = []
 
